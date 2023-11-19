344a1bc5aa60c6dcc31df36cde2a48fdbd027118
Thuyein-Thet <thuyein.thet555@gmail.com>
2023-11-19 12:20:17 +0630

    [Start Learning Java]

---
+++ Simple.java
@@ -0,0 +1,22 @@
+class Simple {
+    public static void main(String args[]){
+        System.out.println("Hello Java");
+    }
+}
+
+
+// valid java main() method
+// public static void main(String[] args)  
+// public static void main(String []args)  
+// public static void main(String args[])  
+// public static void main(String... args)  
+// static public void main(String[] args)  
+// public static final void main(String[] args)  
+// final public static void main(String[] args)  
+// final strictfp public static void main(String[] args)  
+
+// Invalid java main() method
+// public void main(String[] args)  
+// static void main(String[] args)  
+// public void static main(String[] args)  
+// abstract public static void main(String[] args)  
