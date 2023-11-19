308876a2ec769f27a201641c7782c2f0d8c95793
Thuyein-Thet <thuyein.thet555@gmail.com>
2023-11-19 13:02:08 +0630

    [Variable]

---
+++ Narrowing.java
@@ -0,0 +1,9 @@
+public class Narrowing {
+    
+    public static void main(String args[]){
+        float c=10.5f;
+        int a=(int)c;
+        System.out.println(c);
+        System.out.println(a);
+    }
+}
