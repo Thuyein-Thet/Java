1d63613f8757a614b8371d82e139ecb493f6fba9
Thuyein-Thet <thuyein.thet555@gmail.com>
2023-12-03 19:34:36 +0630

    [Array]

---
+++ Array/TestArray2.java
@@ -0,0 +1,9 @@
+class TestArray2{
+    public static void main(String args[]){
+        int arr[] = {33,4,5,3};
+
+        for(int i:arr){
+            System.out.println(i);
+        }
+    }
+}
---
+++ Array/TestArray3.java
@@ -0,0 +1,19 @@
+class TestArray3{
+    
+    static void min(int arr[]){
+        int min = arr[0];
+
+        for(int i=1;i<arr.length;i++){
+            if(min>arr[i]){
+                min = arr[i];
+            }
+        }
+        System.out.println(min);
+    }
+
+    public static void main(String args[]){
+        int a[] = {33,4,26,7,1,5,90};
+
+        min(a);
+    }
+}
